c802193be280838ddb2e3ba62e5a67e1afdde236
kojot <wojtek@herasoft.pl>
2019-10-29 08:09:16 +0100

    simple logic with change name

--- app/src/main/java/com/kojotdev/android/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kojotdev/android/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -26,6 +27,12 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(this, Observer {
             textView.text = it
         })
+
+        val nameText: TextView = root.findViewById(R.id.name_text)
+        val setNameButton: Button = root.findViewById(R.id.set_name_button)
+        setNameButton.setOnClickListener { button ->
+            nameText.text = "Roman"
+        }
         return root
     }
 }
