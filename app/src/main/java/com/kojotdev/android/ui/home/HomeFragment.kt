a90080555dae623e79c2c711cafe6b69b4df34ba
kojot <wojtek@herasoft.pl>
2019-10-29 09:23:30 +0100

    view model navaigation fix

--- app/src/main/java/com/kojotdev/android/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kojotdev/android/ui/home/HomeFragment.kt
@@ -9,19 +9,18 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.navGraphViewModels
 import com.kojotdev.android.R
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private val homeViewModel: HomeViewModel by navGraphViewModels(R.id.mobile_navigation)
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(this, Observer {
